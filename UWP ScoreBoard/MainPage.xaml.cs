4c3eda4d4c91b379ca202e77100ae1a71d0ebd4d
Morais <Morais@ANDREMORAISAB73>
2016-06-27 17:50:02 -0300

    timer test

--- UWP ScoreBoard/MainPage.xaml.cs
+++ UWP ScoreBoard/MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,42 @@ namespace UWP_ScoreBoard
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Stopwatch _stopwatch = new Stopwatch();
+        Timer _timer;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void stopwatch_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+            if (_stopwatch.IsRunning)
+            {
+                _stopwatch.Stop();
+            }
+            else
+            {
+                _stopwatch.Start();
+
+                _timer = new Timer(updateTime, null, 1500, Timeout.Infinite);
+                
+                //stopwatch.Text = _stopwatch.Elapsed.ToString();
+
+            }
+
+        }
+
+        private async void updateTime(object state)
+        {
+            // do some work not connected with UI
+            stopwatch.Text = _stopwatch.Elapsed.ToString();
+
+            //await Window.Current.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+            //    () => {
+            //        stopwatch.Text = _stopwatch.Elapsed.ToString();
+            //});
         }
     }
 }
